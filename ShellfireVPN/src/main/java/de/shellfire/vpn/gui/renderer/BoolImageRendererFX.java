3ef1dff3a220f8c2d87b88092cb8c72b7f883699
Tcheutchoua Steve <tcheutchouasteve@gmail.com>
2018-06-08 09:51:32 +0100

    VpnNavScreen star and boolean image renderers implementation, tidy up progressdialog duplicates

---
+++ ShellfireVPN/src/main/java/de/shellfire/vpn/gui/renderer/BoolImageRendererFX.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.shellfire.vpn.gui.renderer;
+
+import de.shellfire.vpn.Util;
+import de.shellfire.vpn.gui.model.VpnComparisonFXTableModel;
+import de.shellfire.vpn.webservice.model.VpnEntry;
+import javafx.scene.control.TableCell;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.TextAlignment;
+
+/**
+ *
+ * @author TcheutchouaSteve on May 28, 2018
+ */
+ public class BoolImageRendererFX extends TableCell<VpnComparisonFXTableModel, VpnEntry>{
+    Image iconTrue = Util.getImageIconFX("/icons/yes.png");
+    Image iconFalse = Util.getImageIconFX("/icons/no.png");
+
+    @Override
+    public void updateItem(VpnEntry item, boolean empty) {
+        super.updateItem(item, empty); 
+        Boolean b = item.isBool();
+        Image icon = this.getIcon(b);
+        
+        setGraphic(new ImageView(icon));
+        setTextAlignment(TextAlignment.CENTER);
+        
+    }
+
+    
+     public Image getIcon(Boolean bool) {
+        if (bool) {
+            return this.iconTrue;
+        } else {
+            return this.iconFalse;
+        }
+     }
+}
