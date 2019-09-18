caa638da7bb2cbb896019703621172ff05bcdb33
Tcheutchoua Steve <tcheutchouasteve@gmail.com>
2019-09-18 12:24:21 +0100

    UpdateFX entry point set and working

--- ShellfireVPN/src/main/java/de/shellfire/vpn/gui/renderer/StarImageRendererFX.java
+++ ShellfireVPN/src/main/java/de/shellfire/vpn/gui/renderer/StarImageRendererFX.java
@@ -1,117 +1,81 @@
 /*
- * To change this template, choose Tools | Templates
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package de.shellfire.vpn.gui.renderer;
 
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Font;
-import java.util.HashMap;
-
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableCellRenderer;
-
 import de.shellfire.vpn.Util;
-import de.shellfire.vpn.gui.model.ContentPane;
-import de.shellfire.vpn.webservice.model.VpnEntry;
+import de.shellfire.vpn.gui.model.ServerListFXModel;
 import de.shellfire.vpn.webservice.model.VpnStar;
+import java.util.HashMap;
+import javafx.scene.control.TableCell;
+import javafx.scene.image.Image;
+import javafx.geometry.NodeOrientation;
+import javafx.scene.image.ImageView;
+import org.slf4j.Logger;
 
 /**
  *
- * @author bettmenn
+ * @author Tcheutchoua Steve
  */
-public class StarImageRenderer extends DefaultTableCellRenderer {
+public class StarImageRendererFX extends TableCell<ServerListFXModel, VpnStar> {
 
-    JLabel lbl = new JLabel();
-    HashMap<Integer, ImageIcon> icons = new HashMap <Integer, ImageIcon>(); 
-    HashMap<Integer, ImageIcon> iconsSelected = new HashMap <Integer, ImageIcon>(); 
-    HashMap<Integer, ImageIcon> iconsDisabled = new HashMap <Integer, ImageIcon>(); 
-    
-    
+    HashMap<Integer, Image> icons = new HashMap<Integer, Image>();
+    HashMap<Integer, Image> iconsSelected = new HashMap<Integer, Image>();
+    HashMap<Integer, Image> iconsDisabled = new HashMap<Integer, Image>();
+    private static final Logger log = Util.getLogger(StarImageRendererFX.class.getCanonicalName());
+
+    public StarImageRendererFX() {
+        init();
+    }
+
     private void init() {
-      
-      icons.put(1, Util.getImageIcon("/icons/stars/1star.png"));
-      icons.put(2, Util.getImageIcon("/icons/stars/2star.png"));
-      icons.put(3, Util.getImageIcon("/icons/stars/3star.png"));
-      icons.put(4, Util.getImageIcon("/icons/stars/4star.png"));
-      icons.put(5, Util.getImageIcon("/icons/stars/5star.png"));      
+        String baseUrl = "src/main/resources";
+        icons.put(1, Util.getImageIconFX(baseUrl + "/icons/stars/1star.png"));
+        icons.put(2, Util.getImageIconFX(baseUrl + "/icons/stars/2star.png"));
+        icons.put(3, Util.getImageIconFX(baseUrl + "/icons/stars/3star.png"));
+        icons.put(4, Util.getImageIconFX(baseUrl + "/icons/stars/4star.png"));
+        icons.put(5, Util.getImageIconFX(baseUrl + "/icons/stars/5star.png"));
 
-      iconsSelected.put(1, Util.getImageIcon("/icons/stars/1star_selected.png"));
-      iconsSelected.put(2, Util.getImageIcon("/icons/stars/2star_selected.png"));
-      iconsSelected.put(3, Util.getImageIcon("/icons/stars/3star_selected.png"));
-      iconsSelected.put(4, Util.getImageIcon("/icons/stars/4star_selected.png"));
-      iconsSelected.put(5, Util.getImageIcon("/icons/stars/5star_selected.png"));      
+        iconsSelected.put(1, Util.getImageIconFX(baseUrl + "/icons/stars/1star_selected.png"));
+        iconsSelected.put(2, Util.getImageIconFX(baseUrl + "/icons/stars/2star_selected.png"));
+        iconsSelected.put(3, Util.getImageIconFX(baseUrl + "/icons/stars/3star_selected.png"));
+        iconsSelected.put(4, Util.getImageIconFX(baseUrl + "/icons/stars/4star_selected.png"));
+        iconsSelected.put(5, Util.getImageIconFX(baseUrl + "/icons/stars/5star_selected.png"));
 
-      iconsDisabled.put(1, Util.getImageIcon("/icons/stars/1star_disabled.png"));
-      iconsDisabled.put(2, Util.getImageIcon("/icons/stars/2star_disabled.png"));
-      iconsDisabled.put(3, Util.getImageIcon("/icons/stars/3star_disabled.png"));
-      iconsDisabled.put(4, Util.getImageIcon("/icons/stars/4star_disabled.png"));
-      iconsDisabled.put(5, Util.getImageIcon("/icons/stars/5star_disabled.png"));      
+        iconsDisabled.put(1, Util.getImageIconFX(baseUrl + "/icons/stars/1star_disabled.png"));
+        iconsDisabled.put(2, Util.getImageIconFX(baseUrl + "/icons/stars/2star_disabled.png"));
+        iconsDisabled.put(3, Util.getImageIconFX(baseUrl + "/icons/stars/3star_disabled.png"));
+        iconsDisabled.put(4, Util.getImageIconFX(baseUrl + "/icons/stars/4star_disabled.png"));
+        iconsDisabled.put(5, Util.getImageIconFX(baseUrl + "/icons/stars/5star_disabled.png"));
     }
-    
+
     @Override
-    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-      init();
-      setEnabled(table == null || table.isEnabled());
-      lbl.setOpaque(true);                  
-      
-      
-      lbl.setFont(lbl.getFont().deriveFont(Font.PLAIN, Util.getFontSize()));
-        
-      if (isEnabled()) {
-      	lbl.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, ContentPane.colorLightGray));
-      } else {
-      	lbl.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, ContentPane.colorVeryLightGray));
-      }
-      
-      if (isSelected) {
-		if (isEnabled()) {
-			lbl.setForeground(ContentPane.colorDarkGrey);
-		} else {
-			lbl.setForeground(Color.darkGray);
-		}      
-        
-        lbl.setBackground(table.getSelectionBackground());
-      } else {
-			if (isEnabled()) {
-				lbl.setBackground(table.getBackground());
-				lbl.setForeground(table.getForeground());
-			} else {
-				lbl.setBackground(Color.white);
-				lbl.setForeground(Color.lightGray);
-			}     
-      }
-      
-      VpnStar star; 
-      if (value instanceof VpnStar) {
-        star = (VpnStar)value;
-      } else {
-        VpnEntry e = (VpnEntry) value;
-        star = new VpnStar(e.getStar());  
-      }
-      
-      
-        ImageIcon icon = this.getIcon(star, isSelected);
-        lbl.setText(star.getText());
-        lbl.setIcon(icon);
-        lbl.setHorizontalAlignment(JLabel.CENTER);
-        lbl.validate();
-        
-        return lbl;
+    protected void updateItem(VpnStar item, boolean empty) {
+        super.updateItem(item, empty);
+        if (item == null) {
+            log.debug("StarImageRendererFX: Star Image and text could not be rendered");
+            setText("Empty");
+        } else {
+            Image img = this.getIcon(item, isSelected());
+            setGraphic(new ImageView(img));
+            getGraphic().setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
+            //log.debug("StarImageRendererFX: " + item.getNum());
+            setText(item.getText());
+        }
+        //this.setDisable(true);
+        //this.setDisabled(true);
     }
 
-    public ImageIcon getIcon(VpnStar star, boolean isSelected) {
-      if (isSelected)
-        return iconsSelected.get(star.getNum());
-      else if (isEnabled())
-        return icons.get(star.getNum());
-      else
-        return iconsDisabled.get(star.getNum());
+    public Image getIcon(VpnStar star, boolean isSelected) {
+        if (isSelected) {
+            return iconsSelected.get(star.getNum());
+        } //else if (!isDisabled())
+        else if (isDisabled()) {
+            return iconsDisabled.get(star.getNum());
+        } else {
+            return icons.get(star.getNum());
+        }
     }
-
-
 }
