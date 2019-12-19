87e9a96bdbe1ddabb9a8e4f96eee104e2d20eb4b
Tcheutchoua Steve <tcheutchouasteve@gmail.com>
2019-12-19 18:24:19 +0100

    Display current language in settings menu

--- ShellfireVPN/src/main/java/de/shellfire/vpn/gui/controller/SettingsDialogController.java
+++ ShellfireVPN/src/main/java/de/shellfire/vpn/gui/controller/SettingsDialogController.java
@@ -23,10 +23,12 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 import org.slf4j.Logger;
 import org.xnap.commons.i18n.I18n;
 
@@ -34,7 +36,7 @@ import org.xnap.commons.i18n.I18n;
  *
  * @author TcheutchouaSteve
  */
-public class SettingsDialogController implements Initializable{
+public class SettingsDialogController implements Initializable {
 
     @FXML
     private CheckBox saveLoginData;
@@ -51,22 +53,21 @@ public class SettingsDialogController implements Initializable{
     @FXML
     private Label languageLabel;
     @FXML
-    private MenuButton languageMenuButton;
-    @FXML
     private Button saveSettingsButton;
     @FXML
     private Button cancelButton;
-    
+    @FXML
+    private ComboBox<Language> languageComboBox;
+
     private static final I18n I18N = VpnI18N.getI18n();
     private Language currentLanguage;
-    static LinkedList<Language> availableLanguages = VpnI18N.getAvailableTranslations(); 
-    
+    static LinkedList<Language> availableLanguages = VpnI18N.getAvailableTranslations();
+
     private static final Logger log = Util.getLogger(SettingsDialogController.class.getCanonicalName());
+
     public SettingsDialogController() {
     }
-    
-    
-    
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         this.saveLoginData.setText(I18N.tr("Save login data"));
@@ -77,24 +78,19 @@ public class SettingsDialogController implements Initializable{
         this.languageLabel.setText(I18N.tr("Language :"));
         this.saveSettingsButton.setText(I18N.tr("save settings"));
         this.cancelButton.setText(I18N.tr("cancel"));
-        
-        
-        // Display the name of the current language
-        this.languageMenuButton.setText(VpnI18N.getLanguage().getName());
-        log.debug("SettingsDialogController : initialize - currentlanguage is " + this.languageMenuButton.getText());
-
-       initValues();
+        currentLanguage = VpnI18N.getLanguage();
+        initValues();
     }
 
     @FXML
     private void handleSaveLoginData(ActionEvent event) {
-        if (!this.saveLoginData.isDisabled()&& !saveLoginData.isSelected()) {
-          if (!this.loginAutomatically.isDisabled()) {
-            this.loginAutomatically.setSelected(false);
-          }
-          if (!this.saveLoginData.isDisabled()) {
-            this.saveLoginData.setSelected(false);
-          }
+        if (!this.saveLoginData.isDisabled() && !saveLoginData.isSelected()) {
+            if (!this.loginAutomatically.isDisabled()) {
+                this.loginAutomatically.setSelected(false);
+            }
+            if (!this.saveLoginData.isDisabled()) {
+                this.saveLoginData.setSelected(false);
+            }
         }
     }
 
@@ -108,8 +104,8 @@ public class SettingsDialogController implements Initializable{
     @FXML
     private void handleSaveVpnChoice(ActionEvent event) {
         if (!this.saveVpnChoice.isDisabled() && saveVpnChoice.isSelected() && !this.saveLoginData.isDisabled()) {
-        this.saveLoginData.setSelected(true);
-      }
+            this.saveLoginData.setSelected(true);
+        }
     }
 
     @FXML
@@ -125,124 +121,134 @@ public class SettingsDialogController implements Initializable{
     }
 
     @FXML
-    private void hanldeLanguageMenuButton(ActionEvent event) {
+    private void handleSaveSettingsButton(ActionEvent event) {
+        log.debug("Save button has been clicked");
+        // get a handle to the stage
+        Stage stage = (Stage) this.saveSettingsButton.getScene().getWindow();
+        // do what you have to do
+        stage.hide();
+        log.debug("About to call the save function");
+        save();
     }
 
     @FXML
-    private void handleSaveSettingsButton(ActionEvent event) {
-        // get a handle to the stage
-    Stage stage = (Stage) this.saveSettingsButton.getScene().getWindow();
-    // do what you have to do
-    stage.hide();
-    save();
-    
-    }
-    
-    @FXML
     private void handleCancelButton(ActionEvent event) {
         // get a handle to the stage
-    Stage stage = (Stage) this.cancelButton.getScene().getWindow();
-    // do what you have to do
-    stage.hide();
+        Stage stage = (Stage) this.cancelButton.getScene().getWindow();
+        // do what you have to do
+        stage.hide();
     }
-    
-      private void initValues() {
-      VpnProperties props = VpnProperties.getInstance();
-        
+
+    private void initValues() {
+        VpnProperties props = VpnProperties.getInstance();
+
         if (props.getProperty(LoginController.REG_USER, null) != null) {
             this.saveLoginData.setDisable(true);
         } else {
             this.saveLoginData.setSelected(true); // can only be enabled from login dialog
         }
-        
+
         if (props.getBoolean(LoginController.REG_AUTOlogIN, false)) {
-          this.loginAutomatically.setSelected(true);
-        } else if (props.getProperty(LoginController.REG_USER, null) == null){
+            this.loginAutomatically.setSelected(true);
+        } else if (props.getProperty(LoginController.REG_USER, null) == null) {
             this.loginAutomatically.setDisable(true); // disable if login data not remembered, because then it makes no sense
         }
 
         if (props.getInt(VpnSelectDialogController.REG_REMEMBERSELECTION, 0) != 0) {
             this.saveVpnChoice.setSelected(true);
-        } else if (props.getProperty(LoginController.REG_USER, null) == null){
+        } else if (props.getProperty(LoginController.REG_USER, null) == null) {
             this.saveVpnChoice.setDisable(true); // disable if login data not remembered, because then it makes no sense
         }
-        
+
         boolean autoConnect = props.getBoolean(LoginController.REG_AUTOCONNECT, false);
         this.connectAutomatically.setSelected(autoConnect);
-        
+
         boolean autoStart = Client.vpnAutoStartEnabled();
-        this.startOnBoot.setSelected(autoStart);  
+        this.startOnBoot.setSelected(autoStart);
 
         boolean showStatusUrlOnConnect = props.getBoolean(LoginController.REG_SHOWSTATUSURL, false);
         this.showStatusSite.setSelected(showStatusUrlOnConnect);
 
         this.initLanguages();
     }
-      
-       private void initLanguages() {
+
+    private void initLanguages() {
         log.debug("SettingsDialogController: initLanguages - method called");
         LinkedList<Language> languages = VpnI18N.getAvailableTranslations();
-        for (Language language : languages) {
-            MenuItem menu = new MenuItem(language.getName());
-            menu.setOnAction((event)->{
-                log.debug("SettingsDialogController: initLanguages - onAction event for " + VpnI18N.getLanguage().getName());
-                this.currentLanguage = language;
-                this.languageMenuButton.setText(currentLanguage.getName());
-                log.debug("SettingsDialogController: initLanguages - setting language for " + language.getName());
-            });
-            this.languageMenuButton.getItems().add(menu);
-        }
-        languageMenuButton.setText(VpnI18N.getLanguage().getName());
-    } 
-       
+        this.languageComboBox.getItems().addAll(languages);
+
+        languageComboBox.setCellFactory(
+                new Callback<ListView<Language>, ListCell<Language>>() {
+                    @Override
+                    public ListCell<Language> call(ListView<Language> param) {
+                        final ListCell<Language> cell = new ListCell<Language>() {
+                            @Override
+                            public void updateItem(Language item,
+                                    boolean empty) {
+                                super.updateItem(item, empty);
+                                if (item != null) {
+                                    setText(item.getName());
+                                    languageComboBox.setValue(item);
+                                } else {
+                                    setText(null);
+                                }
+                            }
+                        };
+                        return cell;
+                    }
+                });
+        languageComboBox.setValue(currentLanguage);
+    }
+
     private void save() {
         VpnProperties props = VpnProperties.getInstance();
-
+        this.currentLanguage = this.languageComboBox.getValue();
         if (!this.loginAutomatically.isDisabled()) { // Not disabled means the checkbox is enabled.
-          props.setBoolean(LoginController.REG_AUTOlogIN, this.loginAutomatically.isSelected());
+            props.setBoolean(LoginController.REG_AUTOlogIN, this.loginAutomatically.isSelected());
         }
 
-        if (!this.saveLoginData.isDisabled()&& this.saveLoginData.isSelected() == false) {
-          props.remove(LoginController.REG_USER);
-          props.remove(LoginController.REG_PASS);
+        if (!this.saveLoginData.isDisabled() && this.saveLoginData.isSelected() == false) {
+            props.remove(LoginController.REG_USER);
+            props.remove(LoginController.REG_PASS);
         }
 
-        if (!this.saveVpnChoice.isDisabled()&& !this.saveVpnChoice.isSelected()) {
-          props.remove(VpnSelectDialogController.REG_REMEMBERSELECTION);
+        if (!this.saveVpnChoice.isDisabled() && !this.saveVpnChoice.isSelected()) {
+            props.remove(VpnSelectDialogController.REG_REMEMBERSELECTION);
         }
-        
+
         props.setBoolean(LoginController.REG_AUTOCONNECT, this.connectAutomatically.isSelected());
         props.setBoolean(LoginController.REG_SHOWSTATUSURL, this.showStatusSite.isSelected());
-        
+
         if (this.saveVpnChoice.isSelected()) {
-          WebService service = WebService.getInstance();
-          Vpn vpn = service.getVpn();
-          props.setInt(VpnSelectDialogController.REG_REMEMBERSELECTION, vpn.getVpnId());
+            WebService service = WebService.getInstance();
+            Vpn vpn = service.getVpn();
+            props.setInt(VpnSelectDialogController.REG_REMEMBERSELECTION, vpn.getVpnId());
         }
-        
-        
-        if (this.startOnBoot.isSelected()) { 
-          Client.addVpnToAutoStart();
+
+        if (this.startOnBoot.isSelected()) {
+            Client.addVpnToAutoStart();
         } else {
-          Client.removeVpnFromAutoStart();
+            Client.removeVpnFromAutoStart();
         }
-        
 
         Language oldLanguage = VpnI18N.getLanguage();
-        
+
         if (!currentLanguage.equals(oldLanguage)) {
-          VpnI18N.setLanguage(currentLanguage);  
-          log.debug("SettingsDialogController: save() - language changed to " + currentLanguage.getName());
-          Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-                            alert.setHeaderText(I18N.tr("Changed language settings require a restart of Shellfire VPN to take effect. Restart now?"));
-                            alert.setContentText(I18N.tr("Changed language settings require a restart of ShellfireVPN to take effect."));
-                            alert.showAndWait();
+            VpnI18N.setLanguage(currentLanguage);
+            log.debug("SettingsDialogController: save() - language changed to " + currentLanguage.getName());
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setHeaderText(I18N.tr("Changed language settings require a restart of Shellfire VPN to take effect. Restart now?"));
+            alert.setContentText(I18N.tr("Changed language settings require a restart of ShellfireVPN to take effect."));
+            alert.showAndWait();
             Optional<ButtonType> result = alert.showAndWait();
             if ((result.isPresent()) && (result.get() == ButtonType.OK)) {
                 alert.close();
                 LoginController.restart();
-            }         
+            }
         }
     }
 
+    @FXML
+    private void handleLanguageComboBox(ActionEvent event) {
+    }
 }
